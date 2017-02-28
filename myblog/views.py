233a4247e8f60d69ef8458b0f0717cbfe07ada71
Lauren Barker <lauren@cos.io>
2017-02-28 11:02:33 -0500

    remove semi-colons, remove extra spaces, use some django stuff, removed eval, used try, imported F, probably some other stuff

--- myblog/views.py
+++ myblog/views.py
@@ -1,42 +1,26 @@
 from django.http import HttpResponse, JsonResponse
 
-from myblog.models import Post, PostView
+from myblog.models import Post
+from django.core import serializers
+from django.db.models import F #http://stackoverflow.com/questions/44711/django-increment-blog-entry-view-count-by-one-is-this-efficient
 
 
 def getPosts(request):
-    posts_to_return = list();  # Make an empty list
+    posts_to_return = Post.objects.all()  # Make an empty list
 
-    m = 0
-    while True:
-        m = m + 1
-        if m >= Post.objects.count():
-            break;
-
-        l = Post.objects.all()[m];
-
-        posts_to_return.append({
-            'id': l.id,
-            'author': l.author,
-            'title': l.title,
-            'content': l.content,
-            'count': len(PostView.objects.filter(forWhatPost=l.id))
-        });
-
-    # This does work???
-    # return JsonResponse(posts_to_return);
-    return HttpResponse(posts_to_return, content_type='application/json');
+    return HttpResponse(posts_to_return, content_type='application/json')
 
 
+def getAPost(request, post_number):
+        try:
+            int(post_number)
+        except ValueError:
+            raise ValueError('Number required.')
 
-def getAPost(request, yo_numbah):
-    if not isinstance(eval(yo_numbah), int):
-        return HttpResponse("No a number!", status_code=400)
+        # h = PostView()
+        # h.forWhatPost = int(yo_numbah)  # Needs to be an number
+        # h.save()
+        Post.objects.filter(id=post_number).update(views=F('views')+1)
 
-    u = Post.objects.raw('SELECT * FROM myblog_post WHERE id = ' + yo_numbah)[0];
-
-    h = PostView();
-    h.forWhatPost = eval(yo_numbah);#Needs to be an number
-    h.save();
-
-    i = {o: getattr(u, o) for o in ('id', 'author', 'title', 'content')};
-    return JsonResponse(i)
+        return HttpResponse(
+            serializers.serialize("json", Post.objects.filter(id=1)),content_type='application/json')
