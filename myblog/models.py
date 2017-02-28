31ea9f936523d4a31d6641f8aecfce15ef0b9c03
Chris Seto <chriskseto@gmail.com>
2017-02-27 20:19:15 -0500

    My second commit

--- myblog/models.py
+++ myblog/models.py
@@ -5,3 +5,7 @@ class Post(models.Model):
     author = models.TextField()
     title = models.TextField()
     content = models.TextField()
+
+
+class PostView(models.Model):
+    forWhatPost = models.IntegerField()
