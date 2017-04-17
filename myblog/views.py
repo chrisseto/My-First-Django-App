a260bce60015c8bf5d2d1b9060697f99eb4a4aca
Lauren Barker <lauren@cos.io>
2017-04-17 17:22:22 -0400

    Handle improper and non-existent post number

--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+import json
+
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404
 from django.core import serializers
 from django.db.models import F  # http://stackoverflow.com/questions/447117/django-increment-blog-entry-view-count-by-one-is-this-efficient
+from django.forms.models import model_to_dict
 
 from myblog.models import Post
 
@@ -12,12 +16,11 @@ def getPosts(request):
 def getAPost(request, post_number):
     try:
         int(post_number)
-    except ValueError:
-        raise ValueError('Number required.')
+    except (ValueError, TypeError):
+        return HttpResponseBadRequest('Bad post number. Number required.')
 
     Post.objects.filter(id=post_number).update(views=F('views')+1)
 
-    return HttpResponse(
-        serializers.serialize('json', Post.objects.filter(id=post_number)),
-        content_type='application/json'
-    )
+    post = get_object_or_404(Post, id=post_number)
+
+    return HttpResponse(json.dumps(model_to_dict(post)))
